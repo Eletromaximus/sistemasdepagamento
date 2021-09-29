891e0162163375025161157942901e3d7f59c23e
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-09-29 16:29:25 -0300

    feat(adicionando jest, adicionando atualizaçoes): desativando temporariamente o pre commit de teste

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,15 +1,18 @@
+import * as S from './style'
 
 export default function Cadastro () {
   return (
-    <fieldset>
-      <form action=''>
-        <label htmlFor="nome">Nome</label>
-        <input type='text' className='nome' />
-        <label htmlFor="endereco">Endereço</label>
-        <input type='text' className='endereco' />
-        <label htmlFor="Cep">Cep</label>
-        <input type='text' className='cep' />
-      </form>
-    </fieldset>
+    <S.StylePage>
+      <fieldset>
+        <form action=''>
+          <label htmlFor="nome">Nome</label>
+          <input type='text' className='nome' />
+          <label htmlFor="endereco">Endereço</label>
+          <input type='text' className='endereco' />
+          <label htmlFor="Cep">Cep</label>
+          <input type='text' className='cep' />
+        </form>
+      </fieldset>
+    </S.StylePage>
   )
 }
